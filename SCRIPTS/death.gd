1d8b6f6469a291966a09890ddd320a55b8ed26fe
lakshay4798 <jindalnaresh549@gmail.com>
2026-09-17 12:03:56 +0530

    death tscn and gd: player death

---
+++ SCRIPTS/death.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+func _ready() -> void:
+	pass 
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		body.set_physics_process(false)
+		#var anim = body.get_node("AnimatedSprite2D")
+		#anim.play("die")
+		body.get_node("AnimatedSprite2D").visible = false
+		# Find the particle node and trigger the explosion burst
+		var particles = body.get_node("deathparticles")
+		if particles:
+			particles.emitting = true
+		await get_tree().create_timer(2.0).timeout
+		get_tree().reload_current_scene()
--- SCRIPTS/floortrap.gd
+++ SCRIPTS/floortrap.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 @onready var fake : TileMapLayer = $"../TileMapLayer2"
-
+#@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	pass
 
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
+		#anim.animation = "die"
 		fake.collision_enabled = false
 	

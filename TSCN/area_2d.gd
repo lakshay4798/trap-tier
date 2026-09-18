cee25c345036fe46f59569bafe20fdcf7025d497
lakshay4798 <jindalnaresh549@gmail.com>
2026-09-18 11:20:35 +0530

    feat(level): level 2 design , door design

---
+++ TSCN/area_2d.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var door : Area2D = $"../door"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		#door.global_position = Vector2(500, 300)
+		pass

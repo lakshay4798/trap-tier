100d67484aef20eb3a69e79f12f59af5c9a47769
lakshay4798 <jindalnaresh549@gmail.com>
2026-09-21 11:53:38 +0530

    level 1 complete and some mechanism

--- SCRIPTS/doortrigger.gd
+++ SCRIPTS/doortrigger.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var door = $"../door"
+#@onready var hack= $"../door"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +13,4 @@ func _on_body_entered(body: Node2D) -> void:
 		door.global_position = Vector2(-500,-30)
 		await get_tree().create_timer(1).timeout
 		door.global_position = Vector2(0,-30)
+		

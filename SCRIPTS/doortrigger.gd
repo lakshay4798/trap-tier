c403d4af4a875803aef92162508388e042685d8f
lakshay4798 <jindalnaresh549@gmail.com>
2026-09-22 11:38:30 +0530

    feat(level) : level 6 and some mechanism

--- SCRIPTS/doortrigger.gd
+++ SCRIPTS/doortrigger.gd
@@ -1,16 +1,17 @@
 extends Area2D
 
-@onready var door = $"../door"
-#@onready var hack= $"../door"
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+@export var stay_in_trap_position: bool = false
+@export var trap_position: Vector2 = Vector2(-500, -30)
+@export var reset_position: Vector2 = Vector2(0, -30)
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
-		door.global_position = Vector2(-500,-30)
-		await get_tree().create_timer(1).timeout
-		door.global_position = Vector2(0,-30)
-		
+		var door = get_tree().current_scene.find_child("door")
+		if door:
+			door.global_position = trap_position
+			if stay_in_trap_position:
+				queue_free()
+				return
+			await get_tree().create_timer(1.5).timeout
+			door.global_position = reset_position
+			queue_free()
